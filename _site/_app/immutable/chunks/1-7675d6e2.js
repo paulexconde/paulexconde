ba5bf2120051b8462284953fdb92be564fa96ad4
paulexconde <paulexconde20@gmail.com>
2022-09-04 09:44:54 +0800

    Added favicon

--- _site/_app/immutable/chunks/1-7675d6e2.js
+++
@@ -1,1 +0,0 @@
-import{default as e}from"../components/pages/_error.svelte-8990815d.js";import"./index-145ba0af.js";import"./stores-7416de00.js";import"./singletons-1ec3f15c.js";export{e as component};
